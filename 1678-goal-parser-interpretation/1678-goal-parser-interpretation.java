38ec5174dd0fc865a5935c5f7abd578d0d90d2a4
palashgeek <88385665+palashgeek@users.noreply.github.com>
2022-04-29 00:50:51 +0530

    Time: 1 ms (82.50%), Space: 40.5 MB (84.95%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String interpret(String command) {
+        return command.replace("()","o").replace("(al)","al");//next time come up with diff solution without using //                                                                           //predefine method//
+    }
+} 
