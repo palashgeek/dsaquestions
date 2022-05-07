d4180e5549f38a72bc02f702a17fc223c24c170a
palashgeek <88385665+palashgeek@users.noreply.github.com>
2022-05-08 01:03:21 +0530

    Time: 0 ms (100.00%), Space: 53.3 MB (72.53%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -2,16 +2,15 @@ class Solution {
     public int search(int[] nums, int target) {
         
 		// Corner case
-        if(nums == null || nums.length == 0) return -1;
         
-        int low = 0;
-        int high = nums.length-1;
+        int start = 0;
+        int end = nums.length-1;
         
-        while(low <= high){
-            int mid = (high-low)/2 + low;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] < target) low = mid + 1;
-            else high = mid  - 1;
+        while(start <= end){
+            int mid = (end-start)/2 + start;
+            if(nums[mid] == target){ return mid;}
+            if(nums[mid] < target) {start = mid + 1;}
+            else { end = mid  - 1; }
 	    }
         return -1;
     }
